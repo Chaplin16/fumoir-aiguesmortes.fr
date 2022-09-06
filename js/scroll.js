1ff2abe851ba0dd0fb113a53a8f2a56290b8f021
Chaplin16 <chaplin16@orange.fr>
2022-09-06 15:43:01 +0200

    creation scroll.js pour anim sur les noms des plats

---
+++ js/scroll.js
@@ -0,0 +1,14 @@
+const anim = document.getElementById('anim');
+
+//ANIM IN THE SCROLL IN THE DISHES'S NAME
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 850 && window.scrollX < 600) {
+      anim.classList.add("nameDish");  
+    } 
+    else if (window.scrollY > 700 && window.scrollX > 600) {
+        anim.classList.add("nameDish");  
+    }  
+    else{
+        anim.classList.remove("nameDish"); 
+    }
+  }); 
