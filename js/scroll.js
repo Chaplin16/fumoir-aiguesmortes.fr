ec859ec8f566c221f1a20fa9d87aa18c0bbf4bba
Chaplin16 <chaplin16@orange.fr>
2022-09-08 15:27:30 +0200

    info in law.html

--- js/scroll.js
+++ js/scroll.js
@@ -2,10 +2,10 @@ const anim = document.getElementById('anim');
 
 //ANIM IN THE SCROLL IN THE DISHES'S NAME
 window.addEventListener("scroll", () => {
-    if (window.scrollY > 600 && window.scrollX < 600) {
+    if (window.scrollY > 900 && window.scrollX < 600) {
       anim.classList.add("nameDish");  
     } 
-    else if (window.scrollY > 600 && window.scrollX > 600) {
+    else if (window.scrollY > 700 && window.scrollX > 600) {
         anim.classList.add("nameDish");  
     }  
     else{
