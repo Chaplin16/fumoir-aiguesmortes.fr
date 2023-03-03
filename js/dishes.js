b63dca636b7042dae5ace4feababe83df67e41c2
Chaplin16 <chaplin16@orange.fr>
2023-03-03 23:10:17 +0100

    optimisation photo

--- js/dishes.js
+++ js/dishes.js
@@ -1,67 +1,67 @@
 const dishes = [
     {
         id: 1,
-        alt:"artisan macon aigues mortes beton dessus ferraillage",
+        alt:"Régis au fumoir d aigues mortes",
         photo:"images/gallery/delice-saumon-thon-fumes-regis-fumoir.png"
     },
     {
         id: 2,
-        alt:"artisan macon aigues mortes beton dessus ferraillage",
+        alt:"saumon fumé et thon fumé du fumoir d aigues mortes",
         photo:"images/gallery/saumon-fume-thon-aigues-mortes.jpeg"
     },
     {
         id: 3,
-        alt:"artisan macon aigues mortes beton dessus ferraillage",
+        alt:"plat de thon, saumon et espadon fumé",
         photo:"images/gallery/espadon-thon-fumoir-aigues-mortes.png"
     },
     {
         id: 4,
-        alt:"artisan macon aigues mortes beton dessus ferraillage",
+        alt:"mets de Régis, artisan fumeur à Aigues Mortes",
         photo:"images/gallery/saumon-fume-aigues-mortes-excellent.png"
     },
     {
         id: 5,
-        alt:"artisan macon aigues mortes beton dessus ferraillage",
+        alt:"La maison du fumoir avec Régis, maître fumeur",
         photo:"images/gallery/magret-maison-du-fumoir-aigues-mortes.png"
     },
     {
         id: 6,
-        alt:"artisan macon aigues mortes beton dessus ferraillage",
+        alt:"Régis, maitre fumeur à Aigues Mortes",
         photo:"images/gallery/regis-maison-fumoir-aigues-mortes.png"
     },
     {
         id: 7,
-        alt:"artisan macon aigues mortes beton dessus ferraillage",
+        alt:"roti fumé de Régis",
         photo:"images/gallery/roti-fume-aigues-mortes.jpeg"
     },
     {
         id: 8,
-        alt:"artisan macon aigues mortes beton dessus ferraillage",
+        alt:"le fumoir d Aigues Mortes",
         photo:"images/gallery/fumoir-aigues-mortes-regis.png"
     },
     {
         id: 9,
-        alt:"artisan macon aigues mortes beton dessus ferraillage",
+        alt:"saumon fumé excellent chez Régis au fumoir",
         photo:"images/gallery/saumon-maison-fumoir-aigues-mortes.png"
     },
     {
         id: 10,
-        alt:"artisan macon aigues mortes beton dessus ferraillage",
+        alt:"viande et poissons fumés au fumoir d'Aigues Mortes",
         photo:"images/gallery/saumon-fume-mache-aigues-mortes.jpeg"
     },
     {
         id: 11,
-        alt:"artisan macon aigues mortes beton dessus ferraillage",
+        alt:"l'atelier de la maison du fumoir",
         photo:"images/gallery/boutique-maison-fumoir-aigues-morte.png"
     },
     {
         id: 10,
-        alt:"artisan macon aigues mortes beton dessus ferraillage",
+        alt:"saumon poissons et vainde de taureaux fumes",
         photo:"images/gallery/saumon-fume-aigues-mortes-fumoir.png"
     },
     {
         id: 11,
-        alt:"artisan macon aigues mortes beton dessus ferraillage",
+        alt:"excellent plats de poissons fumés chez Regis du fumoir",
         photo:"images/gallery/saumon-prepare-par-regis-fumoir.png"
     },
 ]
